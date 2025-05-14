4d497c6cf5cba017158c089240952621e99bdb40
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-14 10:42:32 -0300

    feat: ✨ adicionar suporte à localização em português no programa principal

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <clocale>
 
 #include "utils/terminal_colors.h"
 
+using namespace std;
+
 int main() {
+    setlocale(LC_ALL, "Portuguese");
+
     std::cout << BLUE << "Hello, World!" << std::endl;
     return 0;
 }
