b300d302c4c0f441a739754627187654edb2e352
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-14 16:51:24 -0300

    feat: ✨ inserir opções conforme os requisitos do projeto
    Essas opções serão implementadas progressivamente.

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,44 @@ inline void NAO_IMPLEMENTADO() {
 int main() {
     setlocale(LC_ALL, "Portuguese");
 
-    const Menu menu;
+    Menu menu;
+
+    /**
+     * Algumas inserções dependem que outros registros em tabelas
+     * diferentes já existam (ex.: uma pessoa deve ter uma cidade associada).
+     * Portanto algumas opções devem ficar desabilitadas até que haja
+     * ao menos 1 registro na(s) tabela(s) de dependência.
+     *
+     * TODO: Implementar lógica para habilitar/desabilitar opções
+     */
+    menu.inserir(new Categoria{"Inserir dados"});
+
+    // Arquivos que não dependem de outros registros
+    menu.inserir(new Opcao{"Gêneros", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Autores", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Cidades", &NAO_IMPLEMENTADO});
+
+    // Arquivos que dependem de outros registros
+    menu.inserir(new Opcao{"Pessoas", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Editoras", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Livros", &NAO_IMPLEMENTADO});
+
+    menu.inserir(new Categoria{"Consultar dados"});
+    menu.inserir(new Opcao{"Gêneros", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Autores", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Cidades", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Pessoas", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Editoras", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Livros", &NAO_IMPLEMENTADO});
+
+    menu.inserir(new Categoria{"Empréstimos"});
+    menu.inserir(new Opcao{"Realizar empréstimo", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Consultar empréstimo", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Devolver livro", &NAO_IMPLEMENTADO});
+
+    menu.inserir(new Opcao{"Listar todos os empréstimos", &NAO_IMPLEMENTADO});
+    menu.inserir(new Opcao{"Listar devoluções em atraso", &NAO_IMPLEMENTADO});
+
     menu.exibir();
 
     return 0;
