3d5999cc8f26a8b93f562d9721f4c23741991ed0
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-15 01:12:29 -0300

    fix: 🩹 adicionar inicialização padrão para a(s) propriedade(s) da classe `Cidade`

--- include/dominio/arquivos/cidade.h
+++ include/dominio/arquivos/cidade.h
@@ -3,15 +3,21 @@
 #include "arquivo.h"
 #include "dominio/excecoes/comuns/excedeu_maximo.h"
 #include "dominio/excecoes/comuns/propriedade_vazia.h"
+#include "utils/terminal_colors.h"
 
 class Cidade final : public Arquivo {
+    constexpr auto SEM_UF = RED "[SEM UF]" RESET;
+
     std::string _nome{};
     std::string _uf{};
 
 public:
     constexpr static auto NOME_CLASSE = "Cidade";
 
-    Cidade(): Arquivo(NOME_CLASSE) { ; }
+    Cidade(): Arquivo(NOME_CLASSE) {
+        this->setNome(RED "[SEM NOME]" RESET);
+        this->setNome(SEM_UF);
+    }
 
     Cidade(const int id, const std::string &nome, const std::string &uf): Arquivo(NOME_CLASSE) {
         this->setId(id);
@@ -35,6 +41,11 @@ public:
             throw PropriedadeVaziaException("UF", NOME_CLASSE);
         if (uf.size() > 2)
             throw ExcedeuMaximoException("UF", NOME_CLASSE, 2, "caracteres");
+
+        if (uf == SEM_UF) {
+            this->_uf = uf;
+            return;
+        }
 
         auto uf_formatado = uf;
         std::ranges::transform(uf, uf_formatado.begin(), ::toupper);
