0297f89d5b352cf888530d3b22bfda8acf9ced00
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-14 23:53:36 -0300

    feat: ✨ adicionar classe `Autor`

---
+++ include/dominio/arquivos/autor.h
@@ -0,0 +1,29 @@
+#ifndef AUTOR_H
+#define AUTOR_H
+#include "arquivo.h"
+#include "dominio/excecoes/comuns/propriedade_vazia.h"
+
+class Autor final : public Arquivo {
+    std::string _nome{};
+
+public:
+    constexpr static auto NOME_CLASSE = "Autor";
+
+    Autor(): Arquivo(NOME_CLASSE) { ; }
+
+    Autor(const int id, const std::string &nome): Arquivo(NOME_CLASSE) {
+        this->setId(id);
+        this->setNome(nome);
+    }
+
+    [[nodiscard]] std::string getNome() const { return this->_nome; }
+
+    void setNome(const std::string &nome) {
+        if (nome.empty())
+            throw PropriedadeVaziaException("Nome", NOME_CLASSE);
+
+        this->_nome = nome;
+    }
+};
+
+#endif //AUTOR_H
