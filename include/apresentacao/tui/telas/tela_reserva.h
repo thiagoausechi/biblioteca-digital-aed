fe6284877a72a790cd8889b9a480cf4bac80ab46
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-15 16:50:28 -0300

    feat: ✨ adicionar a classe `TelaReserva` como fallback para telas vazias

---
+++ include/apresentacao/tui/telas/tela_reserva.h
@@ -0,0 +1,24 @@
+#ifndef TELA_RESERVA_H
+#define TELA_RESERVA_H
+#include <ftxui/dom/elements.hpp>
+
+#include <ftxui/component/component.hpp>
+
+#include "tela.h"
+
+using namespace ftxui;
+
+/*
+ * Reserva ou "fallback" para quando o Renderizador
+ * estiver com a pilha de telas vazia.
+ */
+class TelaReserva final : public Tela {
+public:
+    Component getComponent() override {
+        return Renderer([] {
+            return color(Color::Red, text("Nenhuma tela registrada!"));
+        });
+    };
+};
+
+#endif //TELA_RESERVA_H
