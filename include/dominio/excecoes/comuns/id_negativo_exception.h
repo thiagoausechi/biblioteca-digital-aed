98b0890a4573b425e9ea1b0586ee530f0798ac85
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-14 23:26:14 -0300

    feat: ✨ adicionar exceção `IdNegativoException`

---
+++ include/dominio/excecoes/comuns/id_negativo_exception.h
@@ -0,0 +1,20 @@
+#ifndef ID_NEGATIVO_EXCEPTION_H
+#define ID_NEGATIVO_EXCEPTION_H
+#include <exception>
+#include <string>
+#include <utility>
+
+class IdNegativoException final : public std::exception {
+    std::string _nome_arquivo;
+
+public:
+    explicit IdNegativoException(std::string nome_arquivo)
+        : _nome_arquivo(std::move(nome_arquivo)) {
+    }
+
+    [[nodiscard]] const char *what() const noexcept override {
+        return ("ID de " + _nome_arquivo + " não pode ser negativo.").c_str();
+    }
+};
+
+#endif //ID_NEGATIVO_EXCEPTION_H
