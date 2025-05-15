5b141d6494d78cb7253074e10f8dab3290fc84d6
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-14 23:30:04 -0300

    feat: ✨ implementar classe base abstrata `Arquivo` com validação de ID

---
+++ include/dominio/arquivos/arquivo.h
@@ -0,0 +1,27 @@
+#ifndef ARQUIVO_H
+#define ARQUIVO_H
+
+#include "dominio/excecoes/comuns/id_negativo_exception.h"
+
+// Classe abstrata para arquivos (não pode ser instanciada)
+class abstract Arquivo {
+    std::string _nome_arquivo;
+    int _id = -1;
+
+public:
+    virtual ~Arquivo() = default;
+
+    explicit Arquivo(std::string nome_arquivo)
+        : _nome_arquivo(std::move(nome_arquivo)) {
+    }
+
+    [[nodiscard]] int getId() const { return this->_id; }
+
+    void setId(const int id) {
+        if (id < 0)
+            throw IdNegativoException(_nome_arquivo);
+        this->_id = id;
+    }
+};
+
+#endif //ARQUIVO_H
