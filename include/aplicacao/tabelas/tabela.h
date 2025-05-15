fcc4e6abd5037897363a1bbcf41d8d5e2e3d81b1
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-15 00:23:05 -0300

    feat: ✨ adicionar classe abstrata `Tabela` como interface para Repositório/DAO

---
+++ include/aplicacao/tabelas/tabela.h
@@ -0,0 +1,41 @@
+#ifndef TABELA_H
+#define TABELA_H
+#include <concepts>
+
+#include "dominio/arquivos/arquivo.h"
+
+template<typename T>
+concept TipoDerivadoDeArquivo = std::derived_from<T, Arquivo>;
+
+/*
+ * Tabela aqui representa uma ‘interface’
+ * para "Repositório" ou "DAO" (Data Access Object)
+ *
+ * Em um 'CRUD' (Create, Read, Update, Delete) haveriam
+ * ainda métodos de atualização e remoção. Aqui não se
+ * fazem necessários, visto que a atualização pode ocorrer
+ * utilizando o próprio ponteiro do arquivo, enquanto
+ * a remoção dificultaria a implementação devido à
+ * relação de dependência entre as tabelas/arquivo.
+ */
+template<TipoDerivadoDeArquivo TArquivo>
+class abstract Tabela {
+  int _ultimo_id = 0;
+
+  /**
+   * Requisito 4.1
+   * _ultimo_id++ representa um auto incremento
+   */
+  int _gerarId() {
+    return _ultimo_id++;
+  }
+
+public:
+  virtual ~Tabela() = default;
+
+  virtual TArquivo *inserir(TArquivo *arquivo) = 0;
+
+  virtual std::optional<TArquivo *> buscar(int id_procurado) = 0;
+};
+
+#endif //TABELA_H
