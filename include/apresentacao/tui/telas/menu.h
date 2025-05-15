8403e8e728a4830a21991f5130d2e715bd7dc3b1
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-15 19:21:08 -0300

    feat: ✨ adicionar a classe `TelaMenu` para desenhar o menu na interface

---
+++ include/apresentacao/tui/telas/menu.h
@@ -0,0 +1,25 @@
+#ifndef TELA_MENU_H
+#define TELA_MENU_H
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/component/component.hpp>
+#include "tela.h"
+#include "utils/menu.h"
+
+using namespace ftxui;
+
+class TelaMenu final : public Tela {
+    std::shared_ptr<menu::Menu> _menu;
+
+public:
+    explicit TelaMenu(std::shared_ptr<menu::Menu> menu) : _menu(menu) { ; }
+
+    Component getComponent() override {
+        // const auto& options = _menu->getOpcoes();
+
+        return Renderer([] {
+            return color(Color::Red, text("Menu"));
+        });
+    }
+};
+
+#endif //TELA_MENU_H
