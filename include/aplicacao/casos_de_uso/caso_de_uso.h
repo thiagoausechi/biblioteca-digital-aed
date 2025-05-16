41197f56505921d0950c99a1da81242c0930b0f4
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-16 18:59:39 -0300

    feat: ✨ adicionar especializações da classe CasoDeUso para suportar casos sem TResposta e/ou TPedido

--- include/aplicacao/casos_de_uso/caso_de_uso.h
+++ include/aplicacao/casos_de_uso/caso_de_uso.h
@@ -9,4 +9,31 @@ public:
     virtual TResposta executar(TPedido pedido) = 0;
 };
 
+// Caso de uso sem um TPedido; Portanto o executar() não recebe parâmetros
+template<typename TResposta>
+class CasoDeUso<TResposta, void> {
+public:
+    virtual ~CasoDeUso() = default;
+
+    virtual TResposta executar() = 0;
+};
+
+// Caso de uso sem um TResposta; Portanto o executar() não retorna nada
+template<typename TPedido>
+class CasoDeUso<void, TPedido> {
+public:
+    virtual ~CasoDeUso() = default;
+
+    virtual void executar(TPedido pedido) = 0;
+};
+
+// Caso de uso sem um TResposta e um TPedido; Portanto o executar() não recebe parâmetros e não retorna nada
+template<>
+class CasoDeUso<void, void> {
+public:
+    virtual ~CasoDeUso() = default;
+
+    virtual void executar() = 0;
+};
+
 #endif //CASO_DE_USO_H
