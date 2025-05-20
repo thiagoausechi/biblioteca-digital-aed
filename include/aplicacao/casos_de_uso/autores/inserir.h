1464d9ac18487ef07b4341e4c83d0cb9c8bf0ff6
Lucas Facina <2411600015@fema.edu.br>
2025-05-20 20:37:17 -0300

    feat: ✨ adicionar caso de uso para inserção de autor

---
+++ include/aplicacao/casos_de_uso/autores/inserir.h
@@ -0,0 +1,30 @@
+#ifndef UC_AUTORES_INSERIR_H
+#define UC_AUTORES_INSERIR_H
+#include <memory>
+#include <string>
+
+#include "aplicacao/casos_de_uso/caso_de_uso.h"
+#include "aplicacao/tabelas/tabela.h"
+#include "dominio/arquivos/autor.h"
+
+struct Pedido {
+    std::string nome;
+};
+
+class InserirAutorUC final : public CasoDeUso<void, const Pedido> {
+    std::shared_ptr<Tabela<Autor> > _autores{};
+
+public:
+    explicit InserirAutorUC(std::shared_ptr<Tabela<Autor> > repositorio_autores)
+        : _autores(std::move(repositorio_autores)) {
+    }
+
+    void executar(const Pedido pedido) override {
+        auto novo_autor = std::make_shared<Autor>();
+        novo_autor->setNome(pedido.nome);
+
+        this->_autores->inserir(novo_autor);
+    }
+};
+
+#endif // UC_AUTORES_INSERIR_H
