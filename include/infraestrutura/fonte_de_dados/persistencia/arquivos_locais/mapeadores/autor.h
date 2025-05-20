e501fd96a99e626f3138d4c52f838fc9d8c8225c
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-20 10:30:17 -0300

    feat: ✨ adicionar classe MapeadorAutorParaArquivoLocal para manipulação de autores em arquivos locais

---
+++ include/infraestrutura/fonte_de_dados/persistencia/arquivos_locais/mapeadores/autor.h
@@ -0,0 +1,18 @@
+#ifndef MAPEADOR_ARQUIVOS_LOCAIS_AUTOR_H
+#define MAPEADOR_ARQUIVOS_LOCAIS_AUTOR_H
+#include <fstream>
+#include <memory>
+
+#include "dominio/arquivos/autor.h"
+
+class MapeadorAutorParaArquivoLocal {
+    static void salvar(std::ofstream &arquivo_local, std::shared_ptr<Autor> autor) {
+        throw std::runtime_error("Método \"MapeadorDeAutorParaArquivoLocal::paraArquivoLocal\" não implementado");
+    }
+
+    static std::shared_ptr<Autor> carregar(std::ifstream &arquivo_local) {
+        throw std::runtime_error("Método \"MapeadorDeAutorParaArquivoLocal::deArquivoLocal\" não implementado");
+    }
+};
+
+#endif //MAPEADOR_ARQUIVOS_LOCAIS_AUTOR_H
