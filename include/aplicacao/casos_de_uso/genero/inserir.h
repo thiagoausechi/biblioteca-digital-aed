ada6f5d91199b075526ddf61811262514de15e90
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-20 20:22:31 -0300

    feat: ✨ adicionar caso de uso para inserção de gênero

---
+++ include/aplicacao/casos_de_uso/genero/inserir.h
@@ -0,0 +1,29 @@
+#ifndef UC_GENERO_INSERIR_H
+#define UC_GENERO_INSERIR_H
+#include <string>
+
+#include "dominio/arquivos/genero.h"
+#include "aplicacao/casos_de_uso/caso_de_uso.h"
+#include "aplicacao/tabelas/tabela.h"
+
+struct Pedido {
+    std::string descricao;
+};
+
+class InserirGeneroUC final : public CasoDeUso<void, const Pedido> {
+    std::shared_ptr<Tabela<Genero>> _generos{};
+
+public:
+    explicit InserirGeneroUC(std::shared_ptr<Tabela<Genero>> repositorio_generos)
+        : _generos(std::move(repositorio_generos)) {
+    }
+
+    void executar(const Pedido pedido) override {
+        auto novo_genero = std::make_shared<Genero>();
+        novo_genero->setDescricao(pedido.descricao);
+
+        this->_generos->inserir(novo_genero);
+    }
+};
+
+#endif //UC_GENERO_INSERIR_H
