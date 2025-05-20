a0611be7f7af47b5795269adb402cd3235bb38e5
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-19 23:51:31 -0300

    feat: ✨ adicionar tabela de `generos` à classe Repositorio

--- include/aplicacao/tabelas/repositorio.h
+++ include/aplicacao/tabelas/repositorio.h
@@ -3,11 +3,13 @@
 #include "tabela.h"
 #include "dominio/arquivos/autor.h"
 #include "dominio/arquivos/cidade.h"
+#include "dominio/arquivos/genero.h"
 
 class Repositorio {
 public:
     std::shared_ptr<Tabela<Autor>> autores = nullptr;
     std::shared_ptr<Tabela<Cidade>> cidades = nullptr;
+    std::shared_ptr<Tabela<Genero>> generos = nullptr;
 };
 
 #endif //REPOSITORIOS_H
