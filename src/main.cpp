500081f22471c2af99e01d2f04f2b5958adca215
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-21 14:37:12 -0300

    refactor: ♻️ substituir chamadas diretas para `Make` por `fabrica->criar` na inicialização do renderizador

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "apresentacao/tui/renderizador.h"
 #include "apresentacao/tui/componentes/menu/categoria.h"
 #include "apresentacao/tui/componentes/menu/opcao.h"
+#include "apresentacao/tui/telas/fabrica.h"
 #include "apresentacao/tui/telas/inicio.h"
 #include "apresentacao/tui/telas/tela_reserva.h"
 #include "apresentacao/tui/telas/inserir/genero.h"
@@ -8,35 +9,39 @@
 using namespace std;
 
 int main() {
-    auto renderizador = std::make_shared<Renderizador>(Components{
-        Opcao("Início", Make<TelaInicial>()),
+    std::shared_ptr<Renderizador> renderizador;
+    std::shared_ptr<Repositorio> repositorio = nullptr; // TODO: Implementar Repositório
+    auto fabrica = std::make_shared<FabricaDeTelas>(repositorio, renderizador);
+
+    renderizador = std::make_shared<Renderizador>(Components{
+        Opcao("Início", fabrica->criar<TelaInicial>()),
         Renderer([] { return separatorEmpty(); }),
 
         Categoria("Inserir dados"),
-        Opcao("Gêneros", Make<TelaInserirGenero>()),
-        Opcao("Autores", Make<TelaReserva>()),
-        Opcao("Cidades", Make<TelaReserva>()),
-        Opcao("Pessoas", Make<TelaReserva>()),
-        Opcao("Editoras", Make<TelaReserva>()),
-        Opcao("Livros", Make<TelaReserva>()),
+        Opcao("Gêneros", fabrica->criar<TelaInserirGenero>()),
+        Opcao("Autores", fabrica->criar<TelaReserva>()),
+        Opcao("Cidades", fabrica->criar<TelaReserva>()),
+        Opcao("Pessoas", fabrica->criar<TelaReserva>()),
+        Opcao("Editoras", fabrica->criar<TelaReserva>()),
+        Opcao("Livros", fabrica->criar<TelaReserva>()),
 
         Renderer([] { return separatorEmpty(); }),
         Categoria("Consultar dados"),
-        Opcao("Gêneros", Make<TelaReserva>()),
-        Opcao("Autores", Make<TelaReserva>()),
-        Opcao("Cidades", Make<TelaReserva>()),
-        Opcao("Pessoas", Make<TelaReserva>()),
-        Opcao("Editoras", Make<TelaReserva>()),
-        Opcao("Livros", Make<TelaReserva>()),
+        Opcao("Gêneros", fabrica->criar<TelaReserva>()),
+        Opcao("Autores", fabrica->criar<TelaReserva>()),
+        Opcao("Cidades", fabrica->criar<TelaReserva>()),
+        Opcao("Pessoas", fabrica->criar<TelaReserva>()),
+        Opcao("Editoras", fabrica->criar<TelaReserva>()),
+        Opcao("Livros", fabrica->criar<TelaReserva>()),
 
         Renderer([] { return separatorEmpty(); }),
         Categoria("Empréstimos"),
-        Opcao("Realizar empréstimo", Make<TelaReserva>()),
-        Opcao("Consultar empréstimo", Make<TelaReserva>()),
-        Opcao("Devolver livro", Make<TelaReserva>()),
+        Opcao("Realizar empréstimo", fabrica->criar<TelaReserva>()),
+        Opcao("Consultar empréstimo", fabrica->criar<TelaReserva>()),
+        Opcao("Devolver livro", fabrica->criar<TelaReserva>()),
 
-        Opcao("Listar todos os empréstimos", Make<TelaReserva>()),
-        Opcao("Listar devoluções em atraso", Make<TelaReserva>()),
+        Opcao("Listar todos os empréstimos", fabrica->criar<TelaReserva>()),
+        Opcao("Listar devoluções em atraso", fabrica->criar<TelaReserva>()),
     });
 
     renderizador->renderizar();
