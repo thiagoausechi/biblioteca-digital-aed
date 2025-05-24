a4edfc2cbcf61c8e34738f91fb30a33d0b0969f0
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-24 18:01:42 -0300

    feat: ✨ implementar execução do caso de uso da tela de inserção de cidade

--- include/apresentacao/tui/telas/inserir/cidade.h
+++ include/apresentacao/tui/telas/inserir/cidade.h
@@ -3,6 +3,7 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/component/component.hpp>
 
+#include "aplicacao/casos_de_uso/cidades/inserir.h"
 #include "apresentacao/tui/lib/formularios/campo_de_entrada.h"
 #include "apresentacao/tui/telas/tela.h"
 
@@ -22,7 +23,10 @@ struct FormularioInsercaoCidade {
 
 class TelaInserirCidade final : public Tela {
     constexpr static auto BOTAO_INSERIR = "Inserir cidade";
+    constexpr static auto MSG_SUCESSO = "Cidade inserida com sucesso!";
+
     FormularioInsercaoCidade _dados_formulario;
+    std::shared_ptr<InserirCidade::UseCase> _caso_de_uso;
 
     Component _input_nome;
     Component _input_uf;
@@ -46,19 +50,42 @@ class TelaInserirCidade final : public Tela {
         _dados_formulario = FormularioInsercaoCidade{};
     }
 
+    void _executar_InserirCidadeUC() {
+        try {
+            this->_caso_de_uso->executar({
+                .nome = _dados_formulario.nome.valor,
+                .uf = _dados_formulario.uf.valor,
+            });
+
+            this->_renderizador->mostrarDialogo(
+                OpcoesDoDialog::Sucesso(MSG_SUCESSO)
+            );
+
+            this->_limpar_formulario();
+        } catch (const std::exception &e) {
+            this->_renderizador->mostrarDialogo(
+                OpcoesDoDialog::Erro(e.what())
+            );
+        }
+    }
+
 public:
     explicit TelaInserirCidade()
         : Tela("Formulário para inserção de Cidade") {
     }
 
     void inicializar() override {
+        _caso_de_uso = std::make_shared<InserirCidade::UseCase>(
+            _repositorio->getCidades()
+        );
+
         _input_nome = criarInput(_dados_formulario.nome);
         _input_uf = criarInput(_dados_formulario.uf);
 
         _botao_inserir
                 = Button(
                     BOTAO_INSERIR,
-                    [] { ; },
+                    [this] { this->_executar_InserirCidadeUC(); },
                     ButtonOption::Border()
                 );
 
