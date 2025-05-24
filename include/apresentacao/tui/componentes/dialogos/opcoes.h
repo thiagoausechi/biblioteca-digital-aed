9730bf2a723b8624c3e22ad80dfcf90064adeaee
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-23 21:56:27 -0300

    feat: ✨ adicionar métodos para criar diálogos de erro

--- include/apresentacao/tui/componentes/dialogos/opcoes.h
+++ include/apresentacao/tui/componentes/dialogos/opcoes.h
@@ -11,8 +11,12 @@ using FnFechar = std::function<void()>;
 using FnTransformar = std::function<Element(const FnFechar &)>;
 
 struct OpcoesDoDialog {
+    // @formatter:off
     static OpcoesDoDialog Padrao(const std::string &titulo,
                                  const Element &conteudo);
+    static OpcoesDoDialog Erro(const std::string &mensagem);
+    static OpcoesDoDialog Erro(const Element &conteudo);
+    // @formatter:on
 
     bool mostrar_dialogo = false; // Reservado para controle do Renderizador
     FnTransformar transformar = [](const FnFechar &) -> Element {
@@ -33,6 +37,25 @@ inline OpcoesDoDialog OpcoesDoDialog::Padrao(const std::string &titulo,
                })
                | size(WIDTH, GREATER_THAN, 30)
                | border;
+    };
+
+    return estado;
+}
+
+inline OpcoesDoDialog OpcoesDoDialog::Erro(const std::string &mensagem) {
+    return Erro(text(mensagem));
+}
+
+inline OpcoesDoDialog OpcoesDoDialog::Erro(const Element &conteudo) {
+    OpcoesDoDialog estado;
+    estado.transformar = [conteudo](const FnFechar &) -> Element {
+        return vbox({
+                   text("Erro!") | bold | color(Color::Red),
+                   separator() | color(Color::Red),
+                   conteudo,
+               })
+               | size(WIDTH, GREATER_THAN, 30)
+               | borderStyled(ROUNDED, Color::Red);
     };
 
     return estado;
