66979f7f7a54450a40b2bae1f309acacb924c328
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-24 16:20:04 -0300

    fix: 🚨 refatorar ExcedeuMaximoException para otimizar a mensagem de erro

--- include/dominio/excecoes/comuns/excedeu_maximo.h
+++ include/dominio/excecoes/comuns/excedeu_maximo.h
@@ -5,34 +5,27 @@
 #include <utility>
 
 class ExcedeuMaximoException final : public std::exception {
-    std::string _nome_propriedade;
-    std::string _nome_arquivo;
-    int _qnt_maxima;
-    std::string _unidade;
+    std::string _mensagem;
 
 public:
     explicit ExcedeuMaximoException(
-        std::string propriedade,
-        std::string nome_arquivo,
+        const std::string &propriedade,
+        const std::string &nome_arquivo,
         const int maximo,
-        std::string unidade = ""
-    )
-        : _nome_propriedade(std::move(propriedade))
-          , _nome_arquivo(std::move(nome_arquivo))
-          , _qnt_maxima(maximo)
-          , _unidade(std::move(unidade)) {
+        const std::string &unidade = ""
+    ) {
+        const auto unidade_formatada = unidade.empty() ? "." : " " + unidade + ".";
+        _mensagem = std::format(
+            "Propriedade '{}' de '{}' não deve exceder {}{}",
+            propriedade,
+            nome_arquivo,
+            maximo,
+            unidade_formatada
+        );
     }
 
     [[nodiscard]] const char *what() const noexcept override {
-        const auto unidade_formatada = _unidade.empty() ? "." : " " + _unidade + ".";
-        return ("Propriedade '"
-                + _nome_propriedade
-                + "' de "
-                + _nome_arquivo
-                + " não deve exceder "
-                + std::to_string(_qnt_maxima)
-                + unidade_formatada
-        ).c_str();
+        return _mensagem.c_str();
     }
 };
 #endif //DOMINIO_EXCECOES_COMUNS_EXCEDEU_MAXIMO_H
