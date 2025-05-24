948607755cd4dac033d4aaf623e1284e220e3e7b
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-24 17:54:45 -0300

    feat: ✨ implementar conteúdo da tela de inserção de cidade

--- include/apresentacao/tui/telas/inserir/cidade.h
+++ include/apresentacao/tui/telas/inserir/cidade.h
@@ -31,8 +31,15 @@ class TelaInserirCidade final : public Tela {
     Component _layout;
 
     Element Conteudo() override {
-        return text("Esta tela ainda não foi implementada!")
-               | color(Color::Red);
+        return vbox({
+            hbox(nome(_dados_formulario.nome), _input_nome->Render()),
+            hbox(nome(_dados_formulario.uf), _input_uf->Render()),
+            filler(),
+            hbox({
+                _botao_inserir->Render() | color(Color::GreenLight),
+                filler(),
+            })
+        });
     }
 
     void _limpar_formulario() {
