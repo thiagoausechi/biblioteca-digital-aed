7cd582ce00a0ebf3097594a1c016ccee916ba1a1
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-24 13:22:51 -0300

    feat: ✨ implementar busca binária para encontrar índice de arquivo

---
+++ include/utils/busca_binaria.h
@@ -0,0 +1,37 @@
+#ifndef BUSCA_BINARIA_H
+#define BUSCA_BINARIA_H
+#include <vector>
+
+/*
+  * Implementação da busca binária para encontrar o
+  * índice de um arquivo.
+  *
+  * Aqui poderíamos utilizar o `std::lower_bound`,
+  * mas para fins de aprendizado e entendimento
+  * da lógica, implementamos a busca binária
+  * manualmente.
+  */
+template<typename T>
+static int encontrarIndicePorId(std::vector<std::shared_ptr<T>> arquivos_sequenciais, int id_procurado) {
+    if (arquivos_sequenciais.empty())
+        return -1;
+
+    int inicio = 0;
+    int fim = static_cast<int>(arquivos_sequenciais.size()) - 1;
+
+    while (inicio <= fim) {
+        const int meio = inicio + (fim - inicio) / 2;
+        int idAtual = arquivos_sequenciais[meio]->getId();
+
+        if (idAtual == id_procurado)
+            return meio;
+        if (idAtual < id_procurado)
+            inicio = meio + 1;
+        else
+            fim = meio - 1;
+    }
+
+    return -1;
+}
+
+#endif //BUSCA_BINARIA_H
