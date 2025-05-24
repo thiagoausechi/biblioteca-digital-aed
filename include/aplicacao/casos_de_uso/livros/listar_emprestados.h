3945aa8abbb474c4f39631a33692a7c1050ac947
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-24 17:30:43 -0300

    refactor: ♻️ reorganizar a estrutura da classe ListarLivrosEmprestados para melhor encapsulamento

--- include/aplicacao/casos_de_uso/livros/listar_emprestados.h
+++ include/aplicacao/casos_de_uso/livros/listar_emprestados.h
@@ -6,43 +6,45 @@
 #include "aplicacao/tabelas/tabela.h"
 #include "dominio/arquivos/livro.h"
 
-struct Resposta {
-    // Requisito 6.1
-    std::vector<std::pair<int, std::string>> livros_emprestados;
+namespace ListarLivrosEmprestados {
+    struct Resposta {
+        // Requisito 6.1
+        std::vector<std::pair<int, std::string>> livros_emprestados;
 
-    // Requisito 6.2
-    int quantidade_emprestados;
-    int quantidade_disponiveis;
+        // Requisito 6.2
+        int quantidade_emprestados;
+        int quantidade_disponiveis;
 
-    Resposta()
-        : quantidade_emprestados(0)
-          , quantidade_disponiveis(0) {
-    }
-};
+        Resposta()
+            : quantidade_emprestados(0)
+              , quantidade_disponiveis(0) {
+        }
+    };
 
-// Requisito 6
-class ListarLivrosEmprestadosUC final : public CasoDeUso<Resposta> {
-    std::shared_ptr<Tabela<Livro>> _livros{};
+    // Requisito 6
+    class UseCase final : public CasoDeUso<Resposta> {
+        std::shared_ptr<Tabela<Livro>> _livros{};
 
-public:
-    explicit ListarLivrosEmprestadosUC(std::shared_ptr<Tabela<Livro>> repositorio_livros)
-        : _livros(std::move(repositorio_livros)) {
-    }
-
-    Resposta executar() override {
-        Resposta resposta;
-
-        for (auto livro: _livros->listarTudo()) {
-            if (livro->estaEmprestado()) {
-                resposta.quantidade_emprestados++;
-                resposta.livros_emprestados.emplace_back(livro->getId(), livro->getNome());
-            } else {
-                resposta.quantidade_disponiveis++;
-            }
+    public:
+        explicit UseCase(std::shared_ptr<Tabela<Livro>> repositorio_livros)
+            : _livros(std::move(repositorio_livros)) {
         }
 
-        return resposta;
-    }
-};
+        Resposta executar() override {
+            Resposta resposta;
+
+            for (const auto &livro: _livros->listarTudo()) {
+                if (livro->estaEmprestado()) {
+                    resposta.quantidade_emprestados++;
+                    resposta.livros_emprestados.emplace_back(livro->getId(), livro->getNome());
+                } else {
+                    resposta.quantidade_disponiveis++;
+                }
+            }
+
+            return resposta;
+        }
+    };
+}
 
 #endif //APLICACAO_UC_LIVROS_LISTAR_EMPRESTADOS_H
