9fac6da42bd1802b357a26ec3d6d545043a6f732
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-24 17:30:55 -0300

    refactor: ♻️ reorganizar a estrutura da classe InserirPessoa para melhor encapsulamento

--- include/aplicacao/casos_de_uso/pessoas/inserir.h
+++ include/aplicacao/casos_de_uso/pessoas/inserir.h
@@ -8,39 +8,41 @@
 #include "dominio/arquivos/cidade.h"
 #include "dominio/excecoes/comuns/arquivo_nao_existe.h"
 
-struct Pedido {
-    std::string nome;
-    std::string cpf;
-    std::string endereco;
-    int id_cidade;
-};
+namespace InserirPessoa {
+    struct Pedido {
+        std::string nome;
+        std::string cpf;
+        std::string endereco;
+        int id_cidade;
+    };
 
-// Requisito 2
-class InserirPessoaUC final : public CasoDeUso<void, const Pedido> {
-    std::shared_ptr<Tabela<Pessoa>> _pessoas{};
-    std::shared_ptr<Tabela<Cidade>> _cidades{};
+    // Requisito 2
+    class UseCase final : public CasoDeUso<void, const Pedido> {
+        std::shared_ptr<Tabela<Pessoa>> _pessoas{};
+        std::shared_ptr<Tabela<Cidade>> _cidades{};
 
-public:
-    explicit InserirPessoaUC(
-        std::shared_ptr<Tabela<Pessoa>> repositorio_pessoas,
-        std::shared_ptr<Tabela<Cidade>> repositorio_cidades)
-        : _pessoas(std::move(repositorio_pessoas))
-          , _cidades(std::move(repositorio_cidades)) {
-    }
+    public:
+        explicit UseCase(
+            std::shared_ptr<Tabela<Pessoa>> repositorio_pessoas,
+            std::shared_ptr<Tabela<Cidade>> repositorio_cidades)
+            : _pessoas(std::move(repositorio_pessoas))
+              , _cidades(std::move(repositorio_cidades)) {
+        }
 
-    void executar(const Pedido pedido) override {
-        // Requisito 2.2
-        if (!this->_cidades->buscar(pedido.id_cidade).has_value())
-            throw ArquivoNaoExisteException(pedido.id_cidade, "cidade");
+        void executar(const Pedido pedido) override {
+            // Requisito 2.2
+            if (!this->_cidades->buscar(pedido.id_cidade).has_value())
+                throw ArquivoNaoExisteException(pedido.id_cidade, "cidade");
 
-        auto nova_pessoa = std::make_shared<Pessoa>();
-        nova_pessoa->setNome(pedido.nome);
-        nova_pessoa->setCPF(pedido.cpf);
-        nova_pessoa->setEndereco(pedido.endereco);
-        nova_pessoa->setIdCidade(pedido.id_cidade);
+            auto nova_pessoa = std::make_shared<Pessoa>();
+            nova_pessoa->setNome(pedido.nome);
+            nova_pessoa->setCPF(pedido.cpf);
+            nova_pessoa->setEndereco(pedido.endereco);
+            nova_pessoa->setIdCidade(pedido.id_cidade);
 
-        this->_pessoas->inserir(nova_pessoa);
-    }
-};
+            this->_pessoas->inserir(nova_pessoa);
+        }
+    };
+}
 
 #endif //APLICACAO_UC_PESSOAS_INSERIR_H
