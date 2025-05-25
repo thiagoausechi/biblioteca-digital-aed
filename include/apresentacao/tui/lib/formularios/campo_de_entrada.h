26c677c998451ede3501aa35bf39f9cbe59b6389
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-25 00:57:39 -0300

    feat: ✨ permitir validação numérica no campo de entrada

--- include/apresentacao/tui/lib/formularios/campo_de_entrada.h
+++ include/apresentacao/tui/lib/formularios/campo_de_entrada.h
@@ -13,10 +13,13 @@ struct Campo {
     std::string placeholder;
     std::string valor;
 
+    bool numerico = false;
+
     std::function<void()> ao_mudar_valor = [] {};
     std::function<void()> ao_enviar = [] {};
-    std::function<bool(const Event &evento)> escutar_evento =
-            [](const Event &) { return false; };
+    std::function<bool(const Event &evento)> escutar_evento = nullptr;
+
+    [[nodiscard]] int valor_numerico() const { return std::stoi(valor); }
 };
 
 static Component criarInput(Campo &campo) {
@@ -30,8 +33,14 @@ static Component criarInput(Campo &campo) {
     return Input(
                &campo.valor,
                opcao
-           ) | CatchEvent([campo](const Event &e) {
-               return campo.escutar_evento(e);
+           ) | CatchEvent([campo](const Event &evento) {
+               if (campo.numerico &&
+                   evento.is_character() &&
+                   !std::isdigit(evento.character()[0]))
+                   // Impede de digitar letras
+                   return true;
+
+               return campo.escutar_evento != nullptr ? campo.escutar_evento(evento) : false;
            });
 }
 
