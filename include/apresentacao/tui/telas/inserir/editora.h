683f6488b37d2cafe3ba6e1ffd7b6efd11a1c55d
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-26 12:50:41 -0300

    feat: ✨ implementar propriedades da tela de inserção de editora

--- include/apresentacao/tui/telas/inserir/editora.h
+++ include/apresentacao/tui/telas/inserir/editora.h
@@ -22,16 +22,64 @@ struct FormularioInsercaoEditora {
 };
 
 class TelaInserirEditora final : public Tela {
+    constexpr static auto AVISO_SEM_CIDADE = "Ao menos uma cidade deve ser cadastrada para inserir uma editora.";
+    constexpr static auto BOTAO_INSERIR = "Inserir editora";
+    FormularioInsercaoEditora _dados_formulario;
+
+    Component _input_nome;
+    Component _input_id_cidade;
+    Component _botao_inserir;
+    Component _formulario;
+    Component _layout;
+
+    std::string _cidade_formatada;
+
     Element Conteudo() override {
         return text("Esta tela ainda não foi implementada!")
                | color(Color::Red);
+    }
+
+    void _limpar_formulario() {
+        _dados_formulario = FormularioInsercaoEditora{};
+    }
+
+    void _formatar_cidade() {
+        auto cidade =
+                _repositorio
+                ->getCidades()
+                ->buscar(_dados_formulario.id_cidade.valor_numerico());
+        if (cidade.has_value())
+            _cidade_formatada = cidade->get()->to_string();
+        else
+            _cidade_formatada = "";
     }
 
 public:
     explicit TelaInserirEditora()
         : Tela("Formulário para inserção de Editora`") {}
 
-    void inicializar() override {}
+    void inicializar() override {
+        _cidade_formatada = "";
+        _dados_formulario.id_cidade.ao_enviar = [this] { this->_formatar_cidade(); };
+
+        _input_nome = criarInput(_dados_formulario.nome);
+        _input_id_cidade = criarInput(_dados_formulario.id_cidade);
+
+        _botao_inserir
+                = Button(
+                    BOTAO_INSERIR,
+                    [] {},
+                    ButtonOption::Border()
+                );
+
+        _formulario = Container::Vertical({
+            _input_nome,
+            _input_id_cidade,
+            _botao_inserir
+        });
+
+        Add(_formulario);
+    }
 };
 
 #endif //APRESENTACAO_TELAS_INSERIR_EDITORA_H
