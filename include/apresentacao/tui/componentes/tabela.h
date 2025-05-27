daa775e312fda5762ae2d46046106c787a9e96e5
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-27 11:17:35 -0300

    docs: 💡 adicionar comentários explicativos na classe TabelaComponent

--- include/apresentacao/tui/componentes/tabela.h
+++ include/apresentacao/tui/componentes/tabela.h
@@ -9,16 +9,41 @@ using namespace ftxui;
 // Este componente que encapsula uma ftxui::Table
 class TabelaComponent final : public ComponentBase {
     Element OnRender() override {
+        /*
+         * Originalmente a classe Table do ftxui
+         * não é tratada como um Componente ou
+         * sequer um Element. Portanto, o comportamento
+         * ao tentar renderizá-la diretamente não é
+         * o esperado. Após ela ser renderizada,
+         * o seu conteúdo é perdido.
+         *
+         * Para não ter necessidade de repetirmos
+         * o processo de carregamento dos dados a
+         * cada renderização, simplesmente fazemos
+         * uma cópia da tabela e renderizamos a
+         * cópia no lugar da original.
+         */
         auto copia_tabela = *this->_tabela_base;
         return copia_tabela.Render();
     }
 
 public:
+    /*
+     * A Telas que usam este componente
+     * podem modificar a estilização da
+     * tabela base, por definimos aqui
+     * com acesso público.
+     */
     std::shared_ptr<Table> _tabela_base;
 
     explicit TabelaComponent() : _tabela_base(std::make_shared<Table>()) {}
 
     void definirTabelaVazia(const std::string &nome_arquivo) const {
+        /*
+         * Não é necessário criar uma nova Tabela
+         * utilizando o make_shared, podemos
+         * simplesmente alterar o conteúdo do ponteiro.
+         */
         *this->_tabela_base = Table(std::vector<Elements>{
             {
                 text(std::format(
