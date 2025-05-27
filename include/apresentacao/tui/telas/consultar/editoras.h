6ce6f8bdf37e62ff2e9ed832857cd7a39a26e2b1
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-05-27 12:34:36 -0300

    feat: ✨ implementar lógica de carregamento dos dados na tabela de Editoras

--- include/apresentacao/tui/telas/consultar/editoras.h
+++ include/apresentacao/tui/telas/consultar/editoras.h
@@ -1,13 +1,17 @@
 #ifndef APRESENTACAO_TELAS_CONSULTAR_EDITORAS_H
 #define APRESENTACAO_TELAS_CONSULTAR_EDITORAS_H
+#include <iomanip>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/component/component.hpp>
 
 #include "apresentacao/tui/telas/tela.h"
+#include "apresentacao/tui/componentes/tabela.h"
 
 using namespace ftxui;
 
 class TelaConsultarEditoras final : public Tela {
+    std::shared_ptr<TabelaComponent> _tabela;
+
     Element Conteudo() override {
         return text("Esta tela ainda não foi implementada!")
                | dim
@@ -15,11 +19,58 @@ class TelaConsultarEditoras final : public Tela {
                | hcenter;
     }
 
+    void _carregarTabela() const {
+        auto editoras = this->_repositorio->getEditoras();
+
+        if (editoras->vazia()) {
+            this->_tabela->definirTabelaVazia(Editora::NOME_CLASSE);
+            return;
+        }
+
+        std::vector<Elements> linhas;
+        std::ostringstream id_formatado;
+
+        // Cabeçalho da Tabela
+        linhas.emplace_back(Elements{text(" ID "), text(" Nome"), text(" Cidade"),});
+
+        // Dados da Tabela
+        for (const auto &editora: editoras->listarTudo()) {
+            id_formatado << std::setfill('0') << std::setw(2) << editora->getId();
+            auto cidade =
+                    this->_repositorio
+                    ->getCidades()
+                    ->buscar(editora->getIdCidade())
+                    .value();
+
+            linhas.emplace_back(Elements{
+                text(id_formatado.str()) | center,
+                text(" " + editora->getNome() + " "),
+                text(" " + cidade->to_string() + " "),
+            });
+
+            id_formatado.str("");
+            id_formatado.clear();
+        }
+
+        this->_tabela->definirDadosTabela(linhas);
+
+        // Estilização de colunas específicas
+        this->_tabela->_tabela_base->SelectColumn(1).Decorate(flex);
+        this->_tabela->_tabela_base->SelectColumn(-1).DecorateCells(align_right);
+    }
+
 public:
     explicit TelaConsultarEditoras()
         : Tela("Consulta das Editoras registradas") {}
 
-    void inicializar() override {}
+    void inicializar() override {
+        _tabela = TabelaConsulta();
+        Add(_tabela);
+
+        this->_carregarTabela();
+    }
+
+    void ao_entrar() override { this->_carregarTabela(); }
 };
 
 #endif //APRESENTACAO_TELAS_CONSULTAR_EDITORAS_H
