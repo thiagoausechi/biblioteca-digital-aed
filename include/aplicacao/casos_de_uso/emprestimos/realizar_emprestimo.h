ea9cd2f5402d1bc1ccd226f1f366e0c04bc31832
Lucas Facina <2411600015@fema.edu.br>
2025-05-27 21:14:18 -0300

    feat: ✨ implementar caso de uso para realizar empréstimo de livro

---
+++ include/aplicacao/casos_de_uso/emprestimos/realizar_emprestimo.h
@@ -0,0 +1,75 @@
+#ifndef UC_REALIZAR_EMPRESTIMO_H
+#define UC_REALIZAR_EMPRESTIMO_H
+#include <memory>
+
+#include "aplicacao/casos_de_uso/caso_de_uso.h"
+#include "aplicacao/tabelas/tabela.h"
+#include "dominio/arquivos/autor.h"
+#include "dominio/arquivos/cidade.h"
+#include "dominio/arquivos/editora.h"
+#include "dominio/arquivos/emprestimo.h"
+#include "dominio/arquivos/livro.h"
+#include "dominio/arquivos/pessoa.h"
+#include "dominio/excecoes/comuns/arquivo_nao_existe.h"
+#include "dominio/excecoes/livro/indisponivel.h"
+
+namespace RealizarEmprestimo {
+    struct Pedido {
+        int id_pessoa;
+        int id_livro;
+    };
+
+    // Requisito 4
+    class UseCase final : public CasoDeUso<void, const Pedido> {
+        std::shared_ptr<Tabela<Pessoa> > _pessoas{};
+        std::shared_ptr<Tabela<Cidade> > _cidades{};
+        std::shared_ptr<Tabela<Livro> > _livros{};
+        std::shared_ptr<Tabela<Editora> > _editoras{};
+        std::shared_ptr<Tabela<Autor> > _autores{};
+        std::shared_ptr<Tabela<Emprestimo> > _emprestimos{};
+
+    public:
+        explicit UseCase(
+            std::shared_ptr<Tabela<Pessoa> > pessoas,
+            std::shared_ptr<Tabela<Cidade> > cidades,
+            std::shared_ptr<Tabela<Livro> > livros,
+            std::shared_ptr<Tabela<Editora> > editoras,
+            std::shared_ptr<Tabela<Autor> > autores,
+            std::shared_ptr<Tabela<Emprestimo> > emprestimos)
+            : _pessoas(std::move(pessoas))
+              , _cidades(std::move(cidades))
+              , _livros(std::move(livros))
+              , _editoras(std::move(editoras))
+              , _autores(std::move(autores))
+              , _emprestimos(std::move(emprestimos)) {
+        }
+
+        void executar(const Pedido pedido) override {
+            auto livro = this->_livros->buscar(pedido.id_livro);
+
+            // Requisito 4.2
+            if (!this->_pessoas->buscar(pedido.id_pessoa).has_value())
+                throw ArquivoNaoExisteException(pedido.id_pessoa, Pessoa::NOME_CLASSE);
+
+            // Requisito 4.3
+            if (!livro.has_value())
+                throw ArquivoNaoExisteException(pedido.id_livro, Livro::NOME_CLASSE);
+
+            // Requisito 4.6
+            if (!livro->disponivel())
+                throw LivroIndisponivelException(pedido.id_livro);
+
+            // Requisito 4.5
+            auto emprestimo = std::make_shared<Emprestimo>();
+
+            // Requisito 4.5.1.3
+            livro->emprestar();
+
+            this->_emprestimos->inserir(emprestimo);
+            this->_livros->atualizar(livro);
+        }
+    };
+}
+
+
+#endif //UC_REALIZAR_EMPRESTIMO_H
