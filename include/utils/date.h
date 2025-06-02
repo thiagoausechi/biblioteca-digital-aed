cfc707875b0fbb357ab9d7774cf4aca76f7801c1
Lucas Facina <2411600015@fema.edu.br>
2025-06-02 17:11:01 -0300

    refactor: ♻️  alterar retorno para datas "vazias"/zeradas

--- include/utils/date.h
+++ include/utils/date.h
@@ -3,6 +3,9 @@
 #include <string>
 
 inline std::string formatar_data(const time_t data) {
+    if (data == 0)
+        return "";
+
     const tm *_data = localtime(&data);
     char buffer[80];
     strftime(buffer, 80, "%d/%m/%Y às %Hh%M", _data);
