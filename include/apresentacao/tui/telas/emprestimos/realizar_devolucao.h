adf209b162cd7877797e530edd028900cc6fbed1
Lucas Facina <2411600015@fema.edu.br>
2025-06-02 18:25:00 -0300

    feat: ✨ implementar execução do caso de uso da tela de devolução de livros

--- include/apresentacao/tui/telas/emprestimos/realizar_devolucao.h
+++ include/apresentacao/tui/telas/emprestimos/realizar_devolucao.h
@@ -3,6 +3,9 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/component/component.hpp>
 
+#include "aplicacao/casos_de_uso/emprestimos/realizar_devolucao.h"
+#include "apresentacao/tui/renderizador.h"
+#include "apresentacao/tui/componentes/dialogos/opcoes.h"
 #include "apresentacao/tui/lib/formularios/campo_de_entrada.h"
 #include "apresentacao/tui/telas/tela.h"
 #include "utils/date.h"
@@ -20,8 +23,10 @@ struct FormularioRealizarDevolucao {
 class TelaRealizarDevolucao final : public Tela {
     constexpr static auto AVISO_SEM_EMPRESTIMO = "Deve ter ao menos um empréstimo para realizar devolução.";
     constexpr static auto BOTAO_DEVOLVER = "Devolver";
+    constexpr static auto MSG_SUCESSO = "Devolução realizada com sucesso!";
 
     FormularioRealizarDevolucao _dados_formulario;
+    std::shared_ptr<RealizarDevolucao::UseCase> _caso_de_uso;
 
     Component _input_id_emprestimo;
     Component _botao_devolver;
@@ -126,12 +131,34 @@ class TelaRealizarDevolucao final : public Tela {
         }
     }
 
+    void _executar_RealizarDevolucaoUC() {
+        try {
+            this->_caso_de_uso->executar({
+                .id_emprestimo = _dados_formulario.id_emprestimo.valor_numerico()
+            });
+
+            this->_renderizador->mostrarDialogo(
+                OpcoesDoDialog::Sucesso(MSG_SUCESSO)
+            );
+
+            this->_limpar_formulario();
+        } catch (const std::exception &e) {
+            this->_renderizador->mostrarDialogo(
+                OpcoesDoDialog::Erro(e.what())
+            );
+        }
+    }
+
 public:
     explicit TelaRealizarDevolucao()
         : Tela("Formulário para Devolução do Empréstimo") {
     }
 
     void inicializar() override {
+        _caso_de_uso = std::make_shared<RealizarDevolucao::UseCase>(
+            _repositorio->getEmprestimos(),
+            _repositorio->getLivros()
+        );
         this->_limpar_informacoes();
         _dados_formulario.id_emprestimo.ao_enviar = [this] { this->_atualizar_informacoes(); };
 
@@ -140,8 +167,7 @@ public:
         _botao_devolver
                 = Button(
                     BOTAO_DEVOLVER,
-                    [] {
-                    },
+                    [this] { this->_executar_RealizarDevolucaoUC(); },
                     ButtonOption::Border()
                 );
 
