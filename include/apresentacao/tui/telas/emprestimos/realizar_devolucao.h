82a5c294343db6384e4408b86b9edc6be81b19cb
Lucas Facina <2411600015@fema.edu.br>
2025-06-02 18:16:55 -0300

    feat: ✨ implementar conteúdo da tela de devolução de livro

--- include/apresentacao/tui/telas/emprestimos/realizar_devolucao.h
+++ include/apresentacao/tui/telas/emprestimos/realizar_devolucao.h
@@ -18,6 +18,7 @@ struct FormularioRealizarDevolucao {
 };
 
 class TelaRealizarDevolucao final : public Tela {
+    constexpr static auto AVISO_SEM_EMPRESTIMO = "Deve ter ao menos um empréstimo para realizar devolução.";
     constexpr static auto BOTAO_DEVOLVER = "Devolver";
 
     FormularioRealizarDevolucao _dados_formulario;
@@ -26,27 +27,54 @@ class TelaRealizarDevolucao final : public Tela {
     Component _botao_devolver;
     Component _formulario;
 
+    bool _informacoes_carregadas;
+
     std::string _nome_pessoa;
-    std::string _nome_livro;
-    std::string _nome_editora;
-    std::string _nome_autor;
+    std::string _info_livro;
     std::string _data_emprestimo;
     std::string _data_devolucao;
 
     Element Conteudo() override {
-        return text("Esta tela ainda não foi implementada.")
-               | color(Color::Red);
+        return vbox({
+            // Campo de entrada para ID do empréstimo
+            hbox(
+                nome(_dados_formulario.id_emprestimo),
+                _input_id_emprestimo->Render()
+            ),
+            text(this->_deve_mostrar_aviso_emprestimo()
+                     ? AVISO_SEM_EMPRESTIMO
+                     : "") | color(Color::Red),
+
+            // Informações
+            separatorEmpty(),
+            !_informacoes_carregadas
+                ? text("Aguardando entrada do usuário...")
+                : vbox({
+                    text("Cliente: " + _nome_pessoa),
+                    text("Livro: " + _info_livro),
+                    text("Data Empréstimo: " + _data_emprestimo),
+                    text("Data Devolução: " + _data_devolucao)
+                }),
+
+            // Rodapé da tela
+            filler(),
+            hbox({
+                _botao_devolver->Render() | color(Color::Green),
+                filler(),
+            })
+        });
     }
+
+    bool _deve_mostrar_aviso_emprestimo() { return this->_repositorio->getEmprestimos()->vazia(); }
 
     void _limpar_formulario() {
         _dados_formulario = FormularioRealizarDevolucao();
     }
 
     void _limpar_informacoes() {
+        _informacoes_carregadas = false;
         _nome_pessoa = "";
-        _nome_livro = "";
-        _nome_editora = "";
-        _nome_autor = "";
+        _info_livro = "";
         _data_emprestimo = "";
         _data_devolucao = "";
     }
@@ -81,10 +109,13 @@ class TelaRealizarDevolucao final : public Tela {
                         ->buscar(livro->getIdAutor())
                         .value();
 
+                _informacoes_carregadas = true;
                 _nome_pessoa = pessoa->getNome();
-                _nome_livro = livro->getNome();
-                _nome_editora = editora->getNome();
-                _nome_autor = autor->getNome();
+                _info_livro =
+                        std::format("\"{}\", de {} - Editora: {}",
+                                    livro->getNome(),
+                                    autor->getNome(),
+                                    editora->getNome());
                 _data_devolucao = formatar_data(emprestimo->getDataEfetivaDevolucao());
                 _data_emprestimo = formatar_data(emprestimo->getDataEmprestimo());
             } else {
@@ -101,7 +132,7 @@ public:
     }
 
     void inicializar() override {
-        this -> _limpar_informacoes();
+        this->_limpar_informacoes();
         _dados_formulario.id_emprestimo.ao_enviar = [this] { this->_atualizar_informacoes(); };
 
         _input_id_emprestimo = criarInput(_dados_formulario.id_emprestimo);
@@ -109,14 +140,15 @@ public:
         _botao_devolver
                 = Button(
                     BOTAO_DEVOLVER,
-                    [] {},
+                    [] {
+                    },
                     ButtonOption::Border()
                 );
 
         _formulario = Container::Vertical({
-           _input_id_emprestimo,
-           _botao_devolver
-       });
+            _input_id_emprestimo,
+            _botao_devolver
+        });
 
         Add(_formulario);
     }
